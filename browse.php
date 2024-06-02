a6817597144d11f3e5b26939b8ce910ed5ee899d
thomasbouresECE <133095557+thomasbouresECE@users.noreply.github.com>
2024-06-02 23:42:24 +0200

    Add files via upload

--- browse.php
+++ browse.php
@@ -1,10 +1,9 @@
 <?php
 include 'db.php';
+$category = isset($_GET['category']) ? $_GET['category'] : '';
+$keyword = isset($_GET['keyword']) ? $_GET['keyword'] : '';
 
-$category = $_GET['category'];
-$keyword = $_GET['keyword'];
-
-$sql = "SELECT * FROM Propriete WHERE 1=1";
+$sql = "SELECT * FROM propriete WHERE 1";
 
 if (!empty($category)) {
     $sql .= " AND Type='$category'";
@@ -19,7 +18,9 @@ $result = $conn->query($sql);
 $properties = [];
 
 if ($result->num_rows > 0) {
-    while($row = $result->fetch_assoc()) {
+    while ($row = $result->fetch_assoc()) {
+        $images = explode(';', $row['Images']);
+        $row['Images'] = $images[0]; 
         $properties[] = $row;
     }
 }
